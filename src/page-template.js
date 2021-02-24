259cf770b6f8f4e4eda46ddbafd794556df47c79
ebenezer <e.kankam95@hotmail.com>
2021-02-23 22:50:06 -0600

    added background color to the body and icon to card header

--- src/page-template.js
+++ src/page-template.js
@@ -12,7 +12,7 @@ const profileSection = (data) => {
             <figure class="card">
                 <div class="card-header">
                     <h3 class="name">${member.name}</h3>
-                    <h5 class="job-title">${member.role}</h5>
+                    <h5 class="job-title"><i class="fas fa-mug-hot"></i>${member.role}</h5>
                 </div>
         
                 <figcaption class="card-body">
@@ -36,7 +36,7 @@ const profileSection = (data) => {
             <figure class="card">
                 <div class="card-header">
                     <h3 class="name">${member.name}</h3>
-                    <h5 class="job-title">${member.role}</h5>
+                    <h5 class="job-title"><i class="fas fa-glasses"></i>${member.role}</h5>
                 </div>
         
                 <figcaption class="card-body">
@@ -45,7 +45,7 @@ const profileSection = (data) => {
                         <li class="card-info card-email">Email:<a href="mailto:${member.email}">
                                 ${member.email}</a>
                         </li>
-                        <li class="card-info card-number">office number: ${member.gitHub}</li>
+                        <li class="card-info card-github">GitHub: <a href="https://github.com/${member.gitHub}" target="_blank">${member.gitHub}</a></li>
                     </ul>
                 </figcaption>
             </figure>
@@ -59,7 +59,7 @@ const profileSection = (data) => {
                 <figure class="card">
                     <div class="card-header">
                         <h3 class="name">${member.name}</h3>
-                        <h5 class="job-title">${member.role}</h5>
+                        <h5 class="job-title"><i class="fas fa-user-graduate"></i>${member.role}</h5>
                     </div>
             
                     <figcaption class="card-body">
@@ -86,6 +86,7 @@ const profileSection = (data) => {
 // function to generate an entire webpage
 const templateData = (data) => {
 
+    const currentYear = new Date().getFullYear();
     return `
 
         <!DOCTYPE html>
@@ -111,8 +112,10 @@ const templateData = (data) => {
              
                 </div>
         
+                <footer>&copy; Copyrights ${currentYear} || Created by Ebenezer Kankam</footer>
             </div>
-        
+
+            <script src="https://kit.fontawesome.com/0cb0e7b636.js" crossorigin="anonymous"></script>
         </body>
         
         </html>
